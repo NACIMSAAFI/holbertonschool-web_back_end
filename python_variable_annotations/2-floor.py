dad3f09eaf7efe0c7ac2d0dcffb67b9d006c2feb
NACIMSAAFI <7715@holbertonstudents.com>
2024-04-26 21:19:36 +0100

    Add 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Program use python variabe annotations"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float."""
+    return math.floor(n)
