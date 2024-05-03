a44127af74ac696e99f4fd473fd16a4f0c08d63c
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-03 23:04:32 +0100

    Add 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -9,8 +9,6 @@ from .0-simple_helper_function import index_range
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
-
     def __init__(self):
         self.__dataset = None
 
@@ -21,7 +19,7 @@ class Server:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[:]
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
@@ -38,7 +36,6 @@ class Server:
         """
         assert type(page) == int and page > 0
         assert type(page_size) == int and page_size > 0
-
         dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
         if start_index >= len(dataset):
