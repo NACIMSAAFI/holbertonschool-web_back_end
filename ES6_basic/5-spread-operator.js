acb68ab59b0bfe0246d351c55e292953821b4641
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-07 20:36:58 +0100

    Add 5-spread-operator.js

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,8 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const array3 = array1.concat(array2);
-  const words = string.split('');
-  for (const word of words) {
-    array3.push(word);
-  }
-  return array3;
+  return [...array1, ...array2, ...string];
 }
