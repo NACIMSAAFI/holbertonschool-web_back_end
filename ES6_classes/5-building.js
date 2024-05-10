a8aa730343021152185617ee76bbf9ab242f953b
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-10 20:00:03 +0100

    Add 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,13 @@
 export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    if (this.constructor !== Building
-        && this.evacuationWarningMessage === undefined) {
-      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    constructor(sqft) {
+      if (this.constructor !== Building
+          && this.evacuationWarningMessage === undefined) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+      this._sqft = sqft;
+    }
+  
+    get sqft() {
+      return this._sqft;
     }
   }
-}
