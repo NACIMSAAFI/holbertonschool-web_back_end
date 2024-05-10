f3dd46da06c9b5a1cfb24230ce1c54c17524dfa7
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-10 22:51:44 +0100

    Add 10-car.js

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+const carBrand = Symbol('brand');
+const carMotor = Symbol('motor');
+const carColor = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[carBrand] = brand;
+    this[carMotor] = motor;
+    this[carColor] = color;
+  }
+
+  cloneCar() {
+    return new Car(this[carBrand], this[carMotor], this[carColor]);
+  }
+}
