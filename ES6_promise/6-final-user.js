a4f705da0b75622946cb7952fc3852b238bdc7e9
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-17 22:26:10 +0100

    Add 6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  try {
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
+      { status: userResult.status, value: userResult.value },
+      { status: photoResult.status, value: photoResult.value },
+    ];
+  } catch (error) {
+    return [{ status: 'rejected', value: error.message }];
+  }
+}
