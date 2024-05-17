4c443cbf1061e5525e2a41eadff36e1e2c5c23e4
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-17 21:41:54 +0100

    Add 2-then.js

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(
+    () => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    },
+    () => {
+      console.error('Got a response from the API');
+      return new Error();
+    },
+  );
+}
