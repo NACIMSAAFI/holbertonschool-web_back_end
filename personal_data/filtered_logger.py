04b62b3d1e2fd0bd47c33172ed61ba67d92a1b4a
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-10 20:10:24 +0100

    Task 0

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Function called filter_datum that
+
+returns the log message obfuscated.
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscate specified fields in the log message.
+
+    Args:
+        fields: A list of strings representing fields to obfuscate.
+        redaction: A string representing the obfuscation value.
+        message: The log message containing the fields.
+        separator: The character separating the fields in the log message.
+
+    Returns:
+        The modified log message with specified fields obfuscated.
+    """
+
+    for field in fields:
+        message = re.sub(
+            f"{field}=.*?{separator}",
+            f"{field}={redaction}{separator}",
+            message
+        )
+    return message
