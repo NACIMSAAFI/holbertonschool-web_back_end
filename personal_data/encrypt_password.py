f0bbace98b092555ac3e63e5ee8dd4ff08736694
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-13 19:57:40 +0100

    Task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
 
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
