8645d5408da91b1766cdab89ba6c8f60e5fdec56
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-13 18:23:02 +0100

    Task 3

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,9 @@ Filtered logger for user data.
 from typing import List
 import logging
 import re
+import os
+import mysql.connector
+
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -14,18 +17,7 @@ def filter_datum(
         redaction: str,
         message: str,
         separator: str) -> str:
-    """
-    Obfuscate specified fields in the log message.
-
-    Args:
-        fields: A list of strings representing fields to obfuscate.
-        redaction: A string representing the obfuscation value.
-        message: The log message containing the fields.
-        separator: The character separating the fields in the log message.
-
-    Returns:
-        The modified log message with specified fields obfuscated.
-    """
+    """Obfuscate specified fields in the log message."""
     for field in fields:
         message = re.sub(
             rf"{re.escape(field)}=.*?{re.escape(separator)}",
@@ -36,10 +28,7 @@ def filter_datum(
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-    Uses 'filter_datum' to redact the PII fields in a record message.
-    """
-
+    """Uses 'filter_datum' to redact the PII fields in a record message."""
     REDACTION = "***"
     FORMAT = "[USER_DATA] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
@@ -49,20 +38,18 @@ class RedactingFormatter(logging.Formatter):
         self.fields: List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        Returns the formatted log message filtered
-        to censor sensitive information.
-        """
+        """Returns the formatted log message filtered to censor sensitive
+        information."""
         formatted_msg: str = super().format(record)
         return filter_datum(
-            self.fields, self.REDACTION, formatted_msg, self.SEPARATOR
-        )
+                self.fields,
+                self.REDACTION,
+                formatted_msg,
+                self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """
-    Returns a configured logger object.
-    """
+    """Returns a configured logger object."""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -73,3 +60,34 @@ def get_logger() -> logging.Logger:
 
     logger.addHandler(handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Function to get a connection to the MySQL Database."""
+    try:
+        dbUser = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+        dbPass = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+        dbHost = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+        dbName = os.getenv("PERSONAL_DATA_DB_NAME")
+
+        if not dbName:
+            raise ValueError("Environment variable is not set.")
+
+        dbConnection = mysql.connector.connect(
+            user=dbUser,
+            password=dbPass,
+            host=dbHost,
+            database=dbName
+        )
+
+        if dbConnection.is_connected():
+            return dbConnection
+        else:
+            raise ConnectionError("Failed to connect to the database.")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return None
