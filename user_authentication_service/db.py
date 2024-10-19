87c88a34af09a5b4f8515e8578fb6bafe4493db8
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-19 14:18:34 +0100

    Task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,14 +31,18 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database.
+
+        Args:
+            email (str): The user's email address.
+            hashed_password (str): The user's hashed password.
+
+        Returns:
+            User: The created user instance.
         """
-        Creates a new User object using 'email' and 'hashed_password'
-        correspondingly, adds the data to 'self's DB,
-        and returns it.
-        """
-        RESULT: User = User(
-            email=email,
-            hashed_password=hashed_password,
-        )
-        self._session.add(RESULT)
-        return RESULT
+        user = User(email=email, hashed_password=hashed_password)
+
+        self._session.add(user)
+        self._session.commit()
+
+        return user
