90f6e58cce3e57c8b411129eeab66224adc7a8ba
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-19 14:13:53 +0100

    Task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -42,7 +42,8 @@ class DB:
         """
         user = User(email=email, hashed_password=hashed_password)
 
-        self._session.add(user)
-        self._session.commit()
+        session = self._session
+        session.add(user)
+        session.commit()
 
         return user
