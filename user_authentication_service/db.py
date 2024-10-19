1c2f27280bb9f930f57c73f3fe7f34a90aba30dd
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-19 14:56:20 +0100

    Task 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 from user import Base
@@ -44,5 +46,28 @@ class DB:
 
         self._session.add(user)
         self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments to filter users.
+
+        Returns:
+            User: The first user instance that matches the criteria.
+
+        Raises:
+            InvalidRequestError: If no keyword arguments are provided.
+            NoResultFound: If no user matches the criteria.
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
 
         return user
