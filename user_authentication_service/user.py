fd151b2fd71eec0a3003dad500c8220ddf5c561a
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-19 12:15:12 +0100

    Task 0

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""SQLAlchemy model named User for a database table
+named users (using the mapping declaration of SQLAlchemy)"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,6 +9,17 @@ Base = declarative_base()
 
 
 class User(Base):
+    """Represents a user in the users table.
+
+    Attributes:
+        user_id (int): The primary key for the user.
+        user_email (str): The email of the user;
+        must be unique and non-nullable.
+        user_hashed_password (str): The hashed password of the user;
+        must be non-nullable.
+        user_session_id (str): An optional session ID for the user.
+        user_reset_token (str): An optional reset token for password recovery.
+    """
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True)
