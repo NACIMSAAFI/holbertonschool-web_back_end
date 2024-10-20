05ceb1a92e781ec3789f2c2e63aacf582c77a612
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-20 21:21:30 +0100

    Task 19

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Flask app"""
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
-
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -22,7 +21,7 @@ def register():
     password = request.form.get("password")
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
+        return jsonify({"email": email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
@@ -33,7 +32,7 @@ def login():
     email = request.form.get("email")
     password = request.form.get("password")
     valid = AUTH.valid_login(email, password)
-    if valid is False:
+    if not valid:
         abort(401)
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
@@ -72,14 +71,14 @@ def get_reset_password_token():
     email = request.form.get("email")
     try:
         reset_token = AUTH.get_reset_password_token(email)
-        return (jsonify({"email": email, "reset_token": reset_token}),)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
 
 
-@app.route("/sessions", methods=["DELETE"])
-def logout():
-    """Logout method."""
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """Update user password."""
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
