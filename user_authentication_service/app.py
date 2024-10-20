0c945317b29cebcf2bcf09afbb1ba3b4151db306
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-20 20:27:24 +0100

    Task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -45,12 +45,14 @@ def login():
 def logout():
     """Logout method"""
     session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
-        abort(403)
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            response = redirect("/")
+            response.delete_cookie("session_id")
+            return response
+    abort(403)
 
 
 if __name__ == "__main__":
