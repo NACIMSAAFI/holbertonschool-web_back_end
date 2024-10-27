0938f2d8d2e4027438a985ccdfd004af5a23eae7
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-27 23:50:13 +0100

    Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -4,23 +4,32 @@ from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
 
 app = Flask(__name__)
+babel = Babel(app)
+gettext.__doc__ = "Documentation for gettext"
+""" Checker requirements """
 
-class Config:
-    LANGUAGES = ["en", "fr"]
+
+class Config(object):
+    """ Class will configure available languages in the app """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 
-babel = Babel(app)
-
-@babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/')
 def index():
+    """ Returning our html page """
     return render_template('3-index.html')
 
+
+@babel.localeselector
+def get_locale():
+    """ Getting locale from request.accept_languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
