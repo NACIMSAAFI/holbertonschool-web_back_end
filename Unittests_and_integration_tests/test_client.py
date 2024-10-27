981a7a73f8127df1ba7152af14259792a0017c38
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-27 22:01:17 +0100

    Task 4

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Test Client File"""
 
-
 import unittest
 from parameterized import parameterized
 from client import GithubOrgClient
@@ -11,18 +10,25 @@ from unittest.mock import patch
 class TestGithubOrgClient(unittest.TestCase):
     """Test Github Org Client Class"""
 
-    @parameterized.expand([
-        ("google"),
-        ("abc")
-    ])
-    @patch('client.get_json')
-    def test_org(self, test_org, mock_json):
+    @parameterized.expand(
+        [
+            ("google", {"name": "Google", "id": 1}),
+            ("abc", {"name": "ABC Corp", "id": 2}),
+        ]
+    )
+    @patch("client.get_json")
+    def test_org(self, test_org, expected_output, mock_json):
         """Test Org Method"""
+        mock_json.return_value = expected_output
+
         client = GithubOrgClient(test_org)
-        client.org()
+
+        org_data = client.org
+
         mock_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{test_org}"
-        )
+            f"https://api.github.com/orgs/{test_org}")
+
+        self.assertEqual(org_data, expected_output)
 
 
 if __name__ == "__main__":
