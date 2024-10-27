3ccecbbd50a6bc080e20c7846c3a57cba8ff247c
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-27 21:57:38 +0100

    Task 4

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Test Client File"""
+
+
+import unittest
+from parameterized import parameterized
+from client import GithubOrgClient
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test Github Org Client Class"""
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    @patch('client.get_json')
+    def test_org(self, test_org, mock_json):
+        """Test Org Method"""
+        client = GithubOrgClient(test_org)
+        client.org()
+        mock_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{test_org}"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
