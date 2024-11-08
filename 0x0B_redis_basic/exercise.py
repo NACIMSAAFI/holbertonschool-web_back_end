77dcfdb947f53ba58b6976e3f987123184888c59
NACIMSAAFI <7715@holbertonstudents.com>
2024-11-08 18:27:23 +0100

    Task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -28,3 +28,54 @@ class Cache:
         random_key = str(uuid.uuid4())  # Generate a unique key
         self._redis.set(random_key, data)  # Store data in Redis with the key
         return random_key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Optional[Union[str, bytes, int]]:
+        """
+        Retrieve data from Redis and apply an optional conversion function.
+
+        Args:
+            key (str): The key to retrieve the data.
+            fn (Optional[Callable]): A function to apply to the retrieved data.
+
+        Returns:
+            Optional[Union[str, bytes, int]]: The retrieved data,
+            optionally converted.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        return fn(value) if fn else value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string value from Redis.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[str]: The decoded string value
+            or None if the key does not exist.
+        """
+        value = self._redis.get(key)
+        return value.decode("utf-8") if value else None
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer value from Redis.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[int]: The integer value or None if the conversion fails.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        try:
+            return int(value.decode("utf-8"))
+        except ValueError:
+            return None
