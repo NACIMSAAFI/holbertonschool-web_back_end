2a405372886309b985ce359234251be8c675687d
NACIMSAAFI <7715@holbertonstudents.com>
2024-11-17 20:27:11 +0100

    Task 2

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,34 @@
+// 2-calcul_chai.test.js
+
+import { expect } from 'chai';
+import calculateNumber from './2-calcul_chai.js';
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return the difference of rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return the division of rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when dividing by 0', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+
+  describe('Invalid Operation', () => {
+    it('should throw an error for invalid operation type', () => {
+      expect(() => calculateNumber('MULTIPLY', 2, 3)).to.throw('Invalid operation type');
+    });
+  });
+});
