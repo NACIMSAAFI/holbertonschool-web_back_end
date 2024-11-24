ef794e7700dc1320d457566209317fc209d40fdf
NACIMSAAFI <7715@holbertonstudents.com>
2024-11-24 21:50:07 +0100

    Task 11

---
+++ queuing_system_in_js/8-job.test.js
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+import kue from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  beforeEach(() => {
+    // Enable test mode for the queue
+    queue = kue.createQueue();
+    kue.Job.range(0, -1, 'asc', (err, jobs) => {
+      jobs.forEach((job) => job.remove());
+    });
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    // Clear the queue and exit test mode
+    queue.testMode.clear();
+    queue.testMode.exit();
+  });
+
+  it('should display an error message if jobs is not an array', () => {
+    const invalidJobs = { phoneNumber: '4153518780', message: 'Invalid job' };
+
+    expect(() => createPushNotificationsJobs(invalidJobs, queue)).to.throw(
+      'Jobs is not an array'
+    );
+  });
+
+  it('should create two new jobs in the queue', () => {
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account',
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 5678 to verify your account',
+      },
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    expect(queue.testMode.jobs.length).to.equal(2);
+
+    // Check the contents of the created jobs
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data).to.deep.equal(jobs[0]);
+
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[1].data).to.deep.equal(jobs[1]);
+  });
+});
