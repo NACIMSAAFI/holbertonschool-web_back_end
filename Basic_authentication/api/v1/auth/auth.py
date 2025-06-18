2851c3b3c51a51254fcfe06faf19186af8bd16f5
Nacim Saafi <nacim.saafi@gmail.com>
2025-06-18 21:52:44 +0100

    Task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class to handle authentication and authorization."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determine if authentication is needed for a given path."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns the Authorization header from the request."""
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Returns the current user based on the request."""
+        return None
