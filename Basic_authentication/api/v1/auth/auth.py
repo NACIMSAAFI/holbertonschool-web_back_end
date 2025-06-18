1482065a8e0d16cd46ef98ceca88a68d377ec585
Nacim Saafi <nacim.saafi@gmail.com>
2025-06-18 22:16:29 +0100

    Task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Auth Class """
+"""Auth Class"""
 
 from flask import request
 from typing import List, TypeVar
@@ -10,7 +10,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determine if authentication is needed for a given path."""
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+
+        if not path.endswith("/"):
+            path += "/"
+
+        for excluded_path in excluded_paths:
+            if excluded_path == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns the Authorization header from the request."""
