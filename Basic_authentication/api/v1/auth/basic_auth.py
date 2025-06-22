87d924807c48e6e7ae626e177e33ebbf65cd8370
Nacim Saafi <nacim.saafi@gmail.com>
2025-06-22 22:56:10 +0100

    Task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from api.v1.views.users import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -34,7 +36,8 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str) -> (str, str):
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
         """Extracts user credentials from the decoded Base64 header."""
         if not decoded_base64_authorization_header or not isinstance(
             decoded_base64_authorization_header, str
@@ -46,3 +49,20 @@ class BasicAuth(Auth):
 
         user, password = decoded_base64_authorization_header.split(":", 1)
         return user, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar("User"):
+        """Returns the User object based on email and password."""
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            user = User.search({"email": user_email})
+            if user and user[0].is_valid_password(user_pwd):
+                return user[0]
+        except Exception:
+            return None
+        return None
