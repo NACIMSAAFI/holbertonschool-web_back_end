65894d1181a04ad289b28f02a01a3220339ebab4
Nacim Saafi <nacim.saafi@gmail.com>
2025-06-22 18:42:00 +0100

    Task 6

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -19,6 +19,11 @@ if getenv("AUTH_TYPE") == "auth":
 
     auth = Auth()
 
+elif getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+
+    auth = BasicAuth()
+
 
 @app.before_request
 def before_request() -> None:
