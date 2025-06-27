48f49f03ba609357c84e610c45127dc49afe4d17
Nacim Saafi <nacim.saafi@gmail.com>
2025-06-27 09:13:30 +0100

    Task 3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-""" SessionAuth class for session """
+"""SessionAuth class for session"""
 from api.v1.auth.auth import Auth
 import uuid
 
 
 class SessionAuth(Auth):
-    """ SessionAuth class for session
-    """
+    """SessionAuth class for session"""
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Creates a session ID for a user
+        """Creates a session ID for a user
         Args:
             user_id (str): The user ID to create a session for
         Returns:
@@ -21,3 +21,14 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user ID for a given session ID
+        Args:
+            session_id (str): The session ID to retrieve the user ID for
+        Returns:
+            str: The user ID or None if not found
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
