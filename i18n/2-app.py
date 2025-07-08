abd46407c907b803d18093400984eb45da2ee9fc
Nacim Saafi <nacim.saafi@gmail.com>
2025-07-08 19:33:03 +0100

    Task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
-2-app.py - A Flask application with locale detection.
+2-app.py - A Flask application with locale detection using babel.init_app().
 """
-
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -10,6 +9,7 @@ from flask_babel import Babel
 
 class Config:
     """Configuration class for Flask app."""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -19,20 +19,20 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-babel = Babel(app)
-
-
-@babel.localeselector
 def get_locale():
     """Select the best language match from the user's request."""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/')
+babel = Babel()
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/")
 def index():
     """Render the index page."""
-    return render_template('2-index.html')
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
