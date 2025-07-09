512e88a842d88e87531bacb0f8595ae85c969224
Nacim Saafi <nacim.saafi@gmail.com>
2025-07-09 19:19:53 +0100

    Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
-""" Task 3: Parametrize templates """
+"""Task 3: Parametrize templates"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
-app = Flask(__name__)
-babel = Babel(app)
-gettext.__doc__ = "Documentation for gettext"
-""" Checker requirements """
+from flask_babel import Babel, _
 
 
 class Config(object):
-    """ Class will configure available languages in the app """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Configure available languages in the app"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    """ Returning our html page """
-    return render_template('3-index.html')
-
-
-@babel.localeselector
 def get_locale():
-    """ Getting locale from request.accept_languages """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Determine the best match from the supported languages."""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+babel = Babel()
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def index():
+    """Render the HTML page."""
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
