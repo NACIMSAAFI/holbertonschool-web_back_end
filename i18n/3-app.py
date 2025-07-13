f59efdac97d2e872aeb7c1de52e146265acfb955
Nacim Saafi <nacim.saafi@gmail.com>
2025-07-13 17:20:35 +0100

    Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
 """Task 3: Parametrize templates"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+gettext.__doc__ = "Documentation for gettext"
+""" Checker requirements """
 
 
 class Config(object):
-    """Configure available languages in the app"""
+    """Class will configure available languages in the app"""
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-
-
-def get_locale():
-    """Determine the best match from the supported languages."""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-babel = Babel()
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route("/")
 def index():
-    """Render the HTML page."""
+    """Returning our html page"""
     return render_template("3-index.html")
 
 
+@babel.localeselector
+def get_locale():
+    """Getting locale from request.accept_languages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port="5000")
