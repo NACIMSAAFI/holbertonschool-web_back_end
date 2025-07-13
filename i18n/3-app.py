77e2ebc20bbdfea55b701573ef98f454b9011f8d
Nacim Saafi <nacim.saafi@gmail.com>
2025-07-13 17:26:06 +0100

    Task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 """ Task 3: Parametrize templates """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
-
-app = Flask(__name__)
-babel = Babel(app)
-gettext.__doc__ = "Documentation for gettext"
-""" Checker requirements """
-
+from flask_babel import Babel, _
 
 class Config(object):
     """ Class will configure available languages in the app """
@@ -16,21 +9,20 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
+app = Flask(__name__)
 app.config.from_object(Config)
 
-
-@app.route('/')
-def index():
-    """ Returning our html page """
-    return render_template('3-index.html')
-
+babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
     """ Getting locale from request.accept_languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+@app.route('/')
+def index():
+    """ Returning our html page """
+    return render_template('3-index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
