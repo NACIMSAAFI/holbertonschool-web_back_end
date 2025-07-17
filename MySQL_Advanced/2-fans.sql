40b99c2ace12a96729015e115db6e418f727a1c6
Nacim Saafi <nacim.saafi@gmail.com>
2025-07-17 20:35:05 +0100

    Task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
 -- 2-fans.sql
--- Ranks country origins of bands, ordered by the number of (non-unique) fans
-
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
+-- script that ranks country origins 
+-- of bands, ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
