37b23979fbfb330cfa8d29c29a507a43e7ce91a5
Nacim Saafi <nacim.saafi@gmail.com>
2025-07-21 17:33:18 +0100

    Task 3

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- 3-glam_rock.sql
+-- List all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT 
+    band_name,
+    IFNULL(split, YEAR(CURDATE())) - formed AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY
+    lifespan DESC;
